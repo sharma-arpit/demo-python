c6bf66f9b58337d489b204649f40491fc8e0435a
Arpit Sharma <20788674+s-arpit007@users.noreply.github.com>
2020-10-23 01:28:48 +0530

    Added get methods for attributes

--- helper.py
+++ helper.py
@@ -2,19 +2,27 @@
 class Employee:
 
     def __init__(self, name, psid=None):
-        self.name = name
-        self.psid = psid
-        self.dept = None
+        self._name = name
+        self._psid = psid
+        self._dept = None
 
     def set_dept(self, dept):
-        self.dept = dept
+        self._dept = dept
 
     def set_psid(self, psid):
-        self.psid = psid
+        self._psid = psid
 
     def set_name(self, name):
-        self.name = name
+        self._name = name
 
+    def get_dept(self):
+        return self._dept
+
+    def get_psid(self):
+        return self._psid
+
+    def get_name(self):
+        return self._name
         
     def __str__(self):
         return "Name: " + self.name + ", PSID: " + self.psid + ", DEPT: " + self.dept
